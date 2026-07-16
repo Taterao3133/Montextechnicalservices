2edb6426f3bff16cb61f84ba3457afd94ce40785
Jinkalwar Taterao <taterao@Jinkalwars-MacBook-Air.local>
2026-07-16 20:35:56 +0530

    adds securtity and final production deploying

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,13 +4,12 @@ export const metadata = {
   title: "Montex Technical Services L.L.C | Dubai",
   description:
     "Montex Technical Services L.L.C provides reliable air conditioning, electrical, plumbing, painting, false ceiling, cleaning and maintenance services in Dubai.",
-   icons: {
-    icon: "assets/favicon.ico",
-    shortcut: "assets/favicon.ico",
-    apple: "assets/apple-icon.png",
-    }
-  
-  };
+  icons: {
+    icon: "/icon.png",
+    shortcut: "/favicon.ico",
+    apple: "/apple-icon.png",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
